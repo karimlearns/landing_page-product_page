08d663f6caad49887848bfb04bbcb3d1dc33034d
ABdelkarim <tahari.abdelkarim@gmail.com>
2024-02-02 09:55:17 +0100

    product page

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let video = document.querySelector('#video');
+
+document.querySelector('.button-video').addEventListener("click", (e) => {
+
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
